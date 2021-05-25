e938e42fe85bb135117c68104029d66445178534
zax5021 <zacharyacampbell1@gmail.com>
2021-05-25 00:06:27 -0700

    Revert "Updates"
    This reverts commit a9a1aacdb0d9cb7d467feee32186ae563aaa010d.

---
+++ employeedirectory/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import SearchEmployeeTable from "./components/SearchEmployeeTable";
+
+function App() {
+  return <SearchEmployeeTable/>;
+}
+
+export default App;
---
+++ employeedirectory/src/utils/API.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const BASEURL = "https://randomuser.me/api/?results=5";
+
+const API = {
+  search: function () {
+    return axios.get(BASEURL);
+  },
+};
+
+export default API;
